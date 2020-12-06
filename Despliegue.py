cadbf60aee8fb0c532bb678b7ce62948f539fa8c
Gina Paola Sanabria Maldonado <69398527+gsanabriam@users.noreply.github.com>
2020-12-06 11:42:53 -0500

    Update Despliegue.py

--- Despliegue.py
+++ Despliegue.py
@@ -2,8 +2,6 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
 import pickle
 from skimage import io
 import dash
